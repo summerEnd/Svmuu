b6d5bc403e26091984c2d2633c2afcba35bc2ff6
summerEnd <771509565@qq.com>
2015-05-23 09:34:52 +0800

    commit

--- app/src/main/java/com/yjy998/ui/activity/main/home/HomeFragment.java
+++ app/src/main/java/com/yjy998/ui/activity/main/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.yjy998.ui.activity.main.home;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,7 +22,9 @@ import com.yjy998.common.http.YHttpHandler;
 import com.yjy998.common.util.NumberUtil;
 import com.yjy998.ui.activity.base.BaseFragment;
 import com.yjy998.ui.activity.main.apply.ApplyActivity;
+import com.yjy998.ui.view.GrowNumber;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,9 +38,9 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     private ArrayList<String> images = new ArrayList<String>();
 
     {
-        //todo 换成真实图片
-        images.add("http://a.hiphotos.baidu.com/image/pic/item/eaf81a4c510fd9f9f7604cfb272dd42a2834a428.jpg");
-        images.add("http://e.hiphotos.baidu.com/image/pic/item/1b4c510fd9f9d72abcb25402d62a2834359bbbdf.jpg");
+        //todo 下方仅用于测试，发布请注释
+//        images.add("http://a.hiphotos.baidu.com/image/pic/item/eaf81a4c510fd9f9f7604cfb272dd42a2834a428.jpg");
+//        images.add("http://e.hiphotos.baidu.com/image/pic/item/1b4c510fd9f9d72abcb25402d62a2834359bbbdf.jpg");
     }
 
     private TextView capitalText;
@@ -62,7 +66,6 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
         capitalText = (TextView) findViewById(R.id.capitalText);
         bannerPager = (BannerPager) findViewById(R.id.bannerPager);
         bannerPager.setDotDrawable(R.drawable.home_dot);
-        bannerPager.setTransformer(new TransformerB(images.size()));
         findViewById(R.id.realGame).setOnClickListener(this);
         findViewById(R.id.newMember).setOnClickListener(this);
         findViewById(R.id.myGame).setOnClickListener(this);
@@ -144,11 +147,22 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
             @Override
             protected void onStatusCorrect(Response response) {
                 try {
-                    JSONObject object=new JSONObject(response.data);
+                    JSONObject object = new JSONObject(response.data);
                     capitalText.setText("￥" + NumberUtil.formatStr(object.getString("loanamount")));
-                    JsonUtil.getArray(object.getJSONArray("banners"),String.class,images);
-                    bannerPager.setImageUrls(images);
-                    bannerPager.start();
+                    JSONArray imagesArray = object.getJSONArray("banners");
+
+                    if (imagesArray != null && imagesArray.length() != 0) {
+
+                        for (int i = 0; i < imagesArray.length(); i++) {
+                            images.add(imagesArray.getString(i));
+                        }
+                        bannerPager.setImageUrls(images);
+                        bannerPager.setTransformer(new TransformerB(90));
+                        bannerPager.start();
+                    }
+
+                    String loanamount = object.getString("loanamount");
+                    growNumber(Float.parseFloat(loanamount));
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -156,5 +170,18 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
 
             }
         });
+    }
+
+    void growNumber(float max) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(new LoanAmountText(), "Value", 0, max);
+        animator.setDuration(1300);
+        animator.start();
+    }
+
+    private class LoanAmountText extends GrowNumber {
+        @Override
+        public void setValue(float number) {
+            capitalText.setText("￥" + NumberUtil.format(number));
+        }
     }
 }
