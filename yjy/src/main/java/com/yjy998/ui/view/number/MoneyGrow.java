0c2a9c8fb22a2b91edac3809456b0578651b83aa
Lincoln <771509565@qq.com>
2015-06-16 22:32:53 +0800

    pad适配

--- yjy/src/main/java/com/yjy998/ui/view/number/MoneyGrow.java
+++ yjy/src/main/java/com/yjy998/ui/view/number/MoneyGrow.java
@@ -1,11 +1,12 @@
 package com.yjy998.ui.view.number;
 
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.PropertyValuesHolder;
 import com.sp.lib.common.util.SLog;
 import com.yjy998.common.util.NumberUtil;
 
@@ -27,13 +28,15 @@ public class MoneyGrow extends GrowNumber {
         this.max = max;
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void start() {
-        AnimatorSet set = new AnimatorSet();
+        com.nineoldandroids.animation.AnimatorSet set=new com.nineoldandroids.animation.AnimatorSet();
         ObjectAnimator animator = ObjectAnimator.ofFloat(this, "Value", 0, 1);
         animator.setDuration(1300);
 
         ObjectAnimator scale = ObjectAnimator.ofPropertyValuesHolder(textView, PropertyValuesHolder.ofFloat("ScaleX", 1, 1.4f, 1), PropertyValuesHolder.ofFloat("ScaleY", 1, 1.4f, 1));
         scale.setDuration(500);
+
         set.playSequentially(animator, scale);
         set.start();
     }
