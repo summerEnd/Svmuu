138314b2cc192ccaab801c9c71072f37f8334a0b
Lincoln <771509565@qq.com>
2015-06-17 10:46:08 +0800

    paytest

---
+++ yjy/src/main/java/com/yjy998/ui/activity/pay/PayTestActivity.java
@@ -0,0 +1,18 @@
+package com.yjy998.ui.activity.pay;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.yjy998.R;
+import com.yjy998.ui.activity.base.YJYActivity;
+
+public class PayTestActivity extends YJYActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_test);
+    }
+}
